e2078efdbb8bc9ad9fd85f012cbc72e18518b10e
Shin-sibainu <shincode0712@gmail.com>
2024-12-24 14:47:23 +0900

    ogp mod

--- src/app/post/[slug]/metadata.ts
+++ src/app/post/[slug]/metadata.ts
@@ -5,24 +5,36 @@ export async function generateMetadata({ params }: { params: { slug: string } })
   const post = await getPostBySlug(params.slug);
   
   if (!post) {
-    return {};
+    return {
+      title: "Post not found",
+      description: "The requested post could not be found.",
+    };
   }
 
+  const title = post.title;
+  const description = post.description || "No description available";
+
   return {
-    title: post.title,
-    description: post.description,
+    title,
+    description,
     openGraph: {
-      title: post.title,
-      description: post.description,
+      title,
+      description,
       type: "article",
       publishedTime: post.date,
       authors: post.author ? [post.author] : undefined,
-      images: [`/post/${params.slug}/opengraph-image`],
+      images: [{
+        url: `/post/${params.slug}/opengraph-image`,
+        width: 1200,
+        height: 630,
+        alt: title,
+      }],
     },
     twitter: {
       card: "summary_large_image",
-      title: post.title,
-      description: post.description,
+      title,
+      description,
+      images: [`/post/${params.slug}/opengraph-image`],
     },
   };
 } 
